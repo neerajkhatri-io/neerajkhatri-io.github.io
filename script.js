21696ca3e721a3d0ea8c620505e9a4c65382ec7e
Neeraj Khatri <neeraj.nk.151990@gmail.com>
2026-01-17 22:48:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const cards = document.querySelectorAll('.case-card');
 
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    const isOpen = card.classList.contains('active');
+
+    cards.forEach(c => c.classList.remove('active'));
+
+    if (!isOpen) {
+      card.classList.add('active');
+    }
+  });
+});
